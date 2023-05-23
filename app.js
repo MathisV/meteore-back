b2dada2a8cf3a773bad4c2c94955404ea7ee212f
Mathis Verleene <mathis.verleene@gmail.com>
2023-05-23 22:04:33 +0200

    First commit with express setup

---
+++ app.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+
+import indexRouter from './routes/index.js';
+import usersRouter from './routes/users.js';
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static('./public'));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+export default app;
