d8ebbf09f5f90761ebc6eeef111d61a28eda4a0e
Mathis Verleene <mathis.verleene1@decathlon.com>
2023-06-02 23:18:14 +0200

    Add cors support

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
+import cors from 'cors';
 
 import indexRouter from './routes/index.js';
 import usersRouter from './routes/users.js';
@@ -11,6 +12,7 @@ import forecastRouter from './routes/forecast.js';
 var app = express();
 
 app.use(logger('dev'));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
